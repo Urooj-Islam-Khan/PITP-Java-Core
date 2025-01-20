663b3347ab36c847eff9876d5ea9ade3dde2b722
UROOJ ISLAM KHAN <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2025-01-20 15:05:41 +0500

    Add files via upload

---
+++ StringLec.java
@@ -0,0 +1,12 @@
+public class StringLec {
+
+    public static void main(String[] args) {
+
+        String greetings = "Hello World";
+
+        int length = greetings.length();
+        System.out.println(greetings + " has " + length + " characters");
+        System.out.println(greetings.toUpperCase());
+        System.out.println(greetings.toLowerCase());
+    }
+}
